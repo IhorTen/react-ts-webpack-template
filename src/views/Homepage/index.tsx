11f383643198258ab0d19df41380dbabc1de51d3
IhorTen <tenyachkinigor1992@gmail.com>
2020-02-24 01:09:47 +0200

    add rotation

--- src/views/Homepage/index.tsx
+++ src/views/Homepage/index.tsx
@@ -10,21 +10,48 @@ export interface HomepageState {}
 export default
 class Homepage
 extends React.Component<HomepageProps, HomepageState> {
+    state = {
+        isRotate: false
+    }
+
+    handleRotate = () => {
+        this.setState({isRotate: !this.state.isRotate})
+    }
+
     render() {
+        let { isRotate } = this.state
         return <>
             <main className="v-homepage">
                 <p className="homepage-title">
                     Hi User!
                 </p>
                 <div className="container">
-                    <button className="u-button">
-                        Click Me to start logo anomation!
+                    <button 
+                        className="u-button"
+                        onClick={this.handleRotate}
+                    >
+                        Click Me to {isRotate ? "stop" : "start"} logo anomation!
                     </button>
                     <div className="homepage-content">
-                        This is Template for React+Typescript projects
+                        This is Template for React + Typescript + SASS + Webpack projects
+                        <br/>
+                        <span>
+                            To add routes go to: <code>src/App.tsx</code>
+                        </span>
+                        <br/>
+                        <span>
+                            To create new component go to: <code>src/views/</code>
+                        </span>
+                        <br/>
                     </div>
                     <div className="img-container">
-                        <img src="/static/images/logo.png"/>
+                        <img
+                            className={`${isRotate ? "rotate" : ""}`} 
+                            src="/static/images/logo.png"
+                        />
+                    </div>
+                    <div className="wish">
+                        Happy coding!!
                     </div>
                 </div>
             </main>
